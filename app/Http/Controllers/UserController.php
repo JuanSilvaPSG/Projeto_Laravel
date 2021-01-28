da3d09262f1cec4124328cb1f27bb7113b572030
Juan <juansilvapsg@gmail.com>
2021-01-28 17:59:39 -0300

    Commit 001 Começando com as rotas

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index($id)
+    {
+        # code...
+        return "<h1>Listagem do usuário com código {$id} </h1>" ;
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/form', 'form');
+
+
+/**
+ * Route::verbo-http('URI', 'Controller@método')
+ *
+ * verbo_http = [GET, POST, PUT, PATCH, DELETE, OPTIONS];
+ *
+ * Route::get($uri, $callback);
+ * Route::post($uri, $callback);
+ * Route::put($uri, $callback);
+ * Route::patch($uri, $callback);
+ * Route::delete($uri, $callback);
+ * Route::options($uri, $callback);
+ *
+ * GET: Utilizado para obter dados do servidor e não altera o estado do recurso.
+ *      Quando um formulário GET é disparado, os dados ficam presentes na URL.
+ *
+ * POST: Utilizado para criação de recurso ou envio de dados ao servidor para validação.
+ *       Os dados ficam no corpo da requisição e não na URL.
+ *
+ * Passo a passo: Definir rota -> Criar controllador -> Criação de método -> Camada View
+ */
+
+
+ Route::get('users/{id}', 'App\Http\Controllers\UserController@index');
